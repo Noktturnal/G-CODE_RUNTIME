db2cef1ed17370d1143fd8f2b1544047b240e033
noktturnal <vojta.matt@gmail.com>
2025-02-28 13:06:01 +0100

    core app crfeation, main views moved to core app, runtime_algorithm app added to installed

--- G_CODE_RUNTIME/G_CODE_RUNTIME/urls.py
+++ G_CODE_RUNTIME/G_CODE_RUNTIME/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from .views import home_view
+from ..core.views import home_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
